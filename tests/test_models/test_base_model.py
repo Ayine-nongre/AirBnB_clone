155ea59ce972c83c29f543ca9223c0a3cdf0390e
Ayine-nongre <eugeneatinbire@gmail.com>
2023-10-14 20:55:50 +0000

    Adding documentation to unit test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""Tests"""
+"""Tests cases for the base model"""
 
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 
 class BaseModelTest(unittest.TestCase):
+    """Tests cases for the base model"""
     test_model = BaseModel()
 
     def selfTest(self):
+        """This is to test that an instance
+        is created and saved"""
         self.test_model.name = "Eugene"
         self.test_model.my_number = 43
         self.test_model.save()
@@ -20,6 +23,8 @@ class BaseModelTest(unittest.TestCase):
         self.assertEqual(self.test_model.id, test_modelJson['id'])
 
     def test_save(self):
+        """This is to test the save method
+        in the base model object"""
         self.test_model.name = "Khal"
         self.test_model.save()
         self.assertIsInstance(self.test_model.id, str)
